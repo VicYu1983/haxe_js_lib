389cc450141f6f7cf851b4d3606ce9f6e419550a
vic <finalsteve@hotmail.com>
2015-07-08 18:05:29 +0800

    update

---
+++ bin/js/haxejslib.js
@@ -0,0 +1,52 @@
+(function (console) { "use strict";
+function $extend(from, fields) {
+	function Inherit() {} Inherit.prototype = from; var proto = new Inherit();
+	for (var name in fields) proto[name] = fields[name];
+	if( fields.toString !== Object.prototype.toString ) proto.toString = fields.toString;
+	return proto;
+}
+var Main = function() { };
+Main.main = function() {
+	var wab = Type.createInstance(b2AABB,[]);
+	var b2w = Type.createInstance(b2World,[wab]);
+	b2w.Step();
+	console.log(b2w);
+};
+var Type = function() { };
+Type.createInstance = function(cl,args) {
+	var _g = args.length;
+	switch(_g) {
+	case 0:
+		return new cl();
+	case 1:
+		return new cl(args[0]);
+	case 2:
+		return new cl(args[0],args[1]);
+	case 3:
+		return new cl(args[0],args[1],args[2]);
+	case 4:
+		return new cl(args[0],args[1],args[2],args[3]);
+	case 5:
+		return new cl(args[0],args[1],args[2],args[3],args[4]);
+	case 6:
+		return new cl(args[0],args[1],args[2],args[3],args[4],args[5]);
+	case 7:
+		return new cl(args[0],args[1],args[2],args[3],args[4],args[5],args[6]);
+	case 8:
+		return new cl(args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7]);
+	default:
+		throw new js__$Boot_HaxeError("Too many arguments");
+	}
+	return null;
+};
+var js__$Boot_HaxeError = function(val) {
+	Error.call(this);
+	this.val = val;
+	this.message = String(val);
+	if(Error.captureStackTrace) Error.captureStackTrace(this,js__$Boot_HaxeError);
+};
+js__$Boot_HaxeError.__super__ = Error;
+js__$Boot_HaxeError.prototype = $extend(Error.prototype,{
+});
+Main.main();
+})(typeof console != "undefined" ? console : {log:function(){}});
--- src/Main.hx
+++ src/Main.hx
@@ -11,7 +11,11 @@ class Main
 	
 	static function main() 
 	{
-		
+		//var b2World:Dynamic = untyped __js__( 'b2World' );
+		var wab = Type.createInstance( untyped __js__( 'b2AABB' ), [] );
+		var b2w = Type.createInstance( untyped __js__( 'b2World' ), [ wab ] );
+		b2w.Step();
+		trace( b2w );
 	}
 	
 }
